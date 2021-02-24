78862f6d165107777cc5c2a32c8fa97f09f2b7f9
EduardErnest <edupas001@gmail.com>
2021-02-24 21:56:29 +0000

    Completed exp_runner

---
+++ src/maze/exp_runner.py
@@ -0,0 +1,23 @@
+from maze import Maze
+from policies import random_policy
+
+
+def run_single_exp(envir, policy):
+    obs = envir.reset()
+    _done = False
+
+    total_reward = 0
+
+    while not _done:
+        action = policy(obs)
+        obs, reward, _done = envir.step(action)
+
+        total_reward += reward
+
+    print(envir.turns_elapsed)
+    return total_reward
+
+
+env = Maze(10)
+
+print(run_single_exp(env, random_policy))
