bfe79ed53f3b3e8c6a8b439e8a2585309888425a
Fei Phoon <fei.phoon@gmail.com>
2021-04-08 00:32:27 +0100

    Delete unneeded files

--- src/example.py
+++
@@ -1,6 +0,0 @@
-def docstring_test():
-    """testing inv test works
-    >>> docstring_test()
-    1
-    """
-    return 1
--- tests/test_example.py
+++
@@ -1,9 +0,0 @@
-from example import docstring_test
-
-
-class TestDocstringTest:
-    def test_docstring_test(self):
-        # input = None
-        expected = 1
-        result = docstring_test()
-        assert result == expected
